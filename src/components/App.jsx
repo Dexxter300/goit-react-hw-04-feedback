61cacfdfc2bf8d3bbb579c9fefbe72c5759e3543
Dexxter300 <daniilsafarov2005@gmail.com>
2023-08-10 05:28:25 +0300

    refactoring from class to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,59 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Statistics } from './statistics/statistics';
 import { FeedbackOptions } from './feedbackOptions/feedbackOptions';
 import { Section } from './section/section';
 import { Notification } from './notification/notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    netural: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [netural, setNetural] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positiveFeedbackPercentage, setPositiveFeedbackPercentage] =
+    useState(0);
+
+  const changeStats = option => {
+    if (option === 'good') {
+      setGood(value => value + 1);
+    }
+    if (option === 'netural') {
+      setNetural(value => value + 1);
+    }
+    if (option === 'bad') {
+      setBad(value => value + 1);
+    }
   };
-  total = 0;
-  positiveFeedbackPercentage = 0;
+  useEffect(() => {
+    setTotal(() => good + netural + bad);
+    setPositiveFeedbackPercentage(() => (100 * good) / total);
+  }, [good, netural, bad, total]);
 
-  changeStats = option => {
-    // console.log(event.target.textContent);
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-  };
-
-  countTotalFeedback = () => {
-    console.log(this.state.good);
-    console.log(this.state.netural + 'netural');
-
-    this.total = this.state.good + this.state.netural + this.state.bad + 1;
-    // console.log(this.total);
-    return this.total;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return (100 * this.state.good) / this.countTotalFeedback();
-
-    // setTimeout(
-    //   () =>
-    //     (this.total =
-    //       this.state.good + this.state.netural + this.state.bad + 1),
-    //   100
-    // );
-    // console.log(this.positiveFeedbackPercentage);
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          paddingLeft: '100px',
-        }}
-      >
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.changeStats}
-          ></FeedbackOptions>
-        </Section>
-        <Section title={'Statistic'}>
-          {this.total > 0 ? (
-            <Statistics
-              good={this.state.good}
-              netural={this.state.netural}
-              bad={this.state.bad}
-              total={this.total}
-              positive={this.countPositiveFeedbackPercentage()}
-            ></Statistics>
-          ) : (
-            <Notification message={'There is no feedback'}></Notification>
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        paddingLeft: '100px',
+      }}
+    >
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={['good', 'bad', 'netural']}
+          onLeaveFeedback={changeStats}
+        ></FeedbackOptions>
+      </Section>
+      <Section title={'Statistic'}>
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            netural={netural}
+            bad={bad}
+            total={total}
+            positive={positiveFeedbackPercentage}
+          ></Statistics>
+        ) : (
+          <Notification message={'There is no feedback'}></Notification>
+        )}
+      </Section>
+    </div>
+  );
+};
